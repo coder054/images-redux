af582c3a4b8d7619f9a78971106e69552d4c369b
coder054 <binhdinhgiaftu@gmail.com>
2019-04-11 15:08:00 +0700

    Updates

---
+++ precache-manifest.3be5e06d6eecc03dce9cd8301d493afd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cff3e7689d43cd4a0c65e8027f3f0f9",
+    "url": "/images-redux/static/media/spinner.2cff3e76.gif"
+  },
+  {
+    "revision": "c42a9718ffe084ebf5b9",
+    "url": "/images-redux/static/js/runtime~main.38c9cac1.js"
+  },
+  {
+    "revision": "db6e2595b274fe796a7f",
+    "url": "/images-redux/static/js/main.39b95525.chunk.js"
+  },
+  {
+    "revision": "935e379c8f7d9ac48437",
+    "url": "/images-redux/static/js/2.738baeae.chunk.js"
+  },
+  {
+    "revision": "db6e2595b274fe796a7f",
+    "url": "/images-redux/static/css/main.89d9e276.chunk.css"
+  },
+  {
+    "revision": "935e379c8f7d9ac48437",
+    "url": "/images-redux/static/css/2.cafe327d.chunk.css"
+  },
+  {
+    "revision": "aa7e6141c8379d8c33008ab473a9205d",
+    "url": "/images-redux/index.html"
+  }
+];
